f4804325c5628148e6e85e677c4889986dcb7aa4
Winfred Huang <huang.winfred21@gmail.com>
2021-02-17 14:57:35 -0800

    Add routes for users and transactions

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+
+// routes
+const users = require("./routes/api/users");
+const transactions = require("./routes/api/transactions");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
+app.use("/api/transactions", transactions);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
