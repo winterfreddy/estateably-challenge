c8864dfc952b4d940e603c5fbc82020ac26dcd18
Winfred Huang <huang.winfred21@gmail.com>
2021-02-17 18:55:05 -0800

    Add API routes for transactions

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,6 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const passport = require('passport');
+
+const Transaction = require('../../models/Transaction');
+const validateTransactionInput = require('../../validation/transaction');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the transactions route" }));
+
+// temporary path to grab all transactions from all users
+router.get('/', (req, res) => {
+    Transaction.find()
+        .sort({ date: -1 })
+        .then(transactions => res.json(transactions))
+        .catch(err => res.status(404).json({ notransactionsfound: 'No transactions found' }));
+});
+
+// grab all transactions belonging to that user
+router.get('/user/:accountId', (req, res) => {
+    Transaction.find({accountId: req.params.accountId})
+        .then(transactions => res.json(transactions))
+        .catch(err =>
+            res.status(404).json({ notransactionsfound: 'No transactions found from that user' }
+        )
+    );
+});
+
+router.post('/',
+    passport.authenticate('jwt', { session: false }),
+        (req, res) => {
+            const { errors, isValid } = validateTransactionInput(req.body);
+
+            if (!isValid) {
+                return res.status(400).json(errors);
+            }
+
+            const newTransaction = new Transaction({
+                accountId: req.user.id,
+                category: req.body.category,
+                description: req.body.description,
+                value: req.body.value,
+                choice: req.body.choice
+            });
+
+            newTransaction.save().then(transaction => res.json(transaction));
+        }
+);
 
 module.exports = router;
