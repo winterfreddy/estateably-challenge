d177675b7e96ffe6fa22a7f9ba3e2932f0aff44a
Winfred Huang <huang.winfred21@gmail.com>
2021-02-17 17:04:11 -0800

    Add validation for transactions

---
+++ validation/transaction.js
@@ -0,0 +1,34 @@
+const Validator = require('validator');
+const validText = require("./valid-text");
+
+module.exports = function validateTransactionInput(data) {
+  let errors = {};
+
+  data.description = validText(data.description) ? data.description : '';
+
+  if (Validator.isEmpty(data.category)) {
+    errors.category = "Category is required";
+  }
+
+  if (Validator.isEmpty(data.description)) {
+    errors.description = "Description is required";
+  }
+
+  if (Validator.isEmpty(data.value)) {
+    errors.value = "Value is required";
+  }
+  
+  if (Validator.isDecimal(data.value.toString())) {
+    errors.value = "Value needs to be a decimal number";
+  }
+
+  if (Validator.isEmpty(data.type)) {
+    errors.type = "Type is required";
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  }
+
+}
