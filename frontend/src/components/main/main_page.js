9a5d2661587ffb6775e88744d4799d7cd235534b
Winfred Huang <huang.winfred21@gmail.com>
2021-02-18 12:52:29 -0800

    Allow user authentication on frontend

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Estateably Banking</h1>
+        <footer>
+          Copyright &copy; 2021 Estateably Banking
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
---
+++ frontend/src/components/nav/navbar_container.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+
+import NavBar from './navbar';
+
+const mapStateToProps = state => ({
+    loggedIn: state.session.isAuthenticated,
+    currentUser: state.session.user
+});
+
+const mapDispatchToProps = dispatch => ({
+    logout: () => dispatch(logout())
+});
+
+export default connect( mapStateToProps, mapDispatchToProps )(NavBar);
